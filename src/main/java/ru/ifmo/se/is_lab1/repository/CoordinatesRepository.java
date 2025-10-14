53091167d2917258c594a71be489649c59ad791e
paradisemay <68386299+paradisemay@users.noreply.github.com>
2025-10-14 15:33:04 +0300

    Remove repository for embeddable coordinates

--- src/main/java/ru/ifmo/se/is_lab1/repository/CoordinatesRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.se.is_lab1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ru.ifmo.se.is_lab1.model.Coordinates;
-
-public interface CoordinatesRepository extends JpaRepository<Coordinates, Long> {
-}
