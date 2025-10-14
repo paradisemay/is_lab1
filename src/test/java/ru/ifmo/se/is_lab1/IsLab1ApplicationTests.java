7bf99fd0a2d9aecc7d57db827473df1f1ec95af3
tim <exaple@email.com>
2025-10-14 14:55:02 +0300

    Initial commit

---
+++ src/test/java/ru/ifmo/se/is_lab1/IsLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.se.is_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
