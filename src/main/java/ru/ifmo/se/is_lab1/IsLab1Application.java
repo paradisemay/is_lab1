ef219990abcf865586239fe9f4eaad56ec17b93a
paradisemay <68386299+paradisemay@users.noreply.github.com>
2025-12-01 13:43:04 +0300

    Add retry handling for serializable transaction conflicts

--- src/main/java/ru/ifmo/se/is_lab1/IsLab1Application.java
+++ src/main/java/ru/ifmo/se/is_lab1/IsLab1Application.java
@@ -2,8 +2,10 @@ package ru.ifmo.se.is_lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class IsLab1Application {
 
     public static void main(String[] args) {
